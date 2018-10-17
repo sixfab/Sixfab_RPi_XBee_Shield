c131caf4584d3ca550836e154a1bcb47398186c3
Yasin Kaya <yasinkaya.121@gmail.com>
2018-10-17 17:05:15 +0300

    revised samples

--- sample/receiver.py
+++ sample/receiver.py
@@ -5,10 +5,15 @@
 from xbee import xbee
 import time
 
+# default serial_port='/dev/ttyS0'
+# default serial_baudrate=9600
+# if your xbee configuraiton is different, initialize like below:
+# node = xbee.XBee(serial_port='[your_value]',serial_baudrate=[your_value]) 
 node = xbee.XBee()
 
 # receive if any received message via XBee
 while(1):
-	message = node.receiveString()
-	if(message):
-		print(message)
+	if(node.serialAvailable()):
+		message = node.receiveString()
+		if(message):
+			print(message)
--- sample/sender.py
+++ sample/sender.py
@@ -5,11 +5,18 @@
 from xbee import xbee
 import time
 
+# default serial_port='/dev/ttyS0'
+# default serial_baudrate=9600
+# if your xbee configuraiton is different, initialize like below:
+# node = xbee.XBee(serial_port='[your_value]',serial_baudrate=[your_value]) 
 node = xbee.XBee()
+
+counter = 0
+
 # send a message every 3 seconds
 while(1):
 	message = "hello " + str(counter)
 	node.sendString(message)
-	counter++
+	counter += 1
 	time.sleep(3)
 
